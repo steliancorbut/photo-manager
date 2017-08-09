4d2d837d745dc2d3bd63844c305e2c9cf0303234
steliancorbut <stelian.corbut@gmail.com>
2017-08-09 17:43:25 +0300

    enter description for photos

--- photo-list/photo-list.component.js
+++ photo-list/photo-list.component.js
@@ -20,5 +20,35 @@ angular.
       self.review = {};
       };
 
+      self.newPhoto = {
+        "photoId": 3,
+        "photodate": 2014, 
+        "imageUrl": "img/photos/super-happy-familly.jpg", 
+        "location": "enter location", 
+        "tags": "enter tags",
+        "description": "enter a long description",
+        "reviews": [{
+        "stars": 1,
+        "body": "Not OK",
+        "author": "john.dean2@example.org"
+        }]
+      }
+
+      self.hideform = true;
+      self.showPhotoForm = function(){
+        self.hideform = false;
+      };
+
+      self.addPhoto = function(){
+        self.newPhoto.photoId = 33;
+        self.newPhoto.reviews = [{
+        "stars": 1,
+        "body": "Not OK",
+        "author": "john.dean33@example.org"
+        }];
+        self.photos.push(self.newPhoto);
+        self.hideform = true;
+      };
+
     }]
   });
